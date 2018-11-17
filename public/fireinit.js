70149735c14a2d9ca18f5b3942c2167bebfec2f5
oni <miskw99@gmail.com>
2018-11-17 17:52:09 +0900

    restore default

---
+++ public/fireinit.js
@@ -0,0 +1,25 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyAPZPG4maUipAbULFJ5fl-O2pOs_KbWzBU",
+    authDomain: "onibase-aa4ed.firebaseapp.com",
+    databaseURL: "https://onibase-aa4ed.firebaseio.com",
+    projectId: "onibase-aa4ed",
+    storageBucket: "onibase-aa4ed.appspot.com",
+    messagingSenderId: "453674709726"
+};
+firebase.initializeApp(config);
+
+/*
+var admin = require("firebase-admin");
+
+admin.initializeApp({
+    credential: admin.credential.cert({
+        projectId: 'onibase-aa4ed',
+        clientEmail : 'firebase-adminsdk-z2ams@onibase-aa4ed.iam.gserviceaccount.com',
+        privateKey : '-----BEGIN PRIVATE KEY-----\nMIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEAAoIBAQCdYqzuiOvyN1yt\nJgA7+ckjXpAAgUHeVvzaXAosS8PI1DhJzNtCSu/esiN5wlbkY7AW+Tfp6CpLvuhE\nHqvmS6be12z1TaT1VgM6yreyaU4zN7gHtrTuaIvezNrk8cu+NepJY8FUaOJ5eddQ\n9L86VQVQkmAzWRtiL5fkvoMSxVQ/lJIiIDidlZifhtf30BwkL+ocluGRX65A9Nw/\nXPBFT1R4xsrRyyymu/HHHU0PKe5PkEZy5R2TC5ZvNuRMr5bO8rPteH0igewe2iFD\nEp396rmBsdDYhHS/mLaFYmPT24paVsNIwJnkYLh10/ZAYMjNybanp7Xp8bkBHGTF\nljaEAw1jAgMBAAECggEASf6Gh29gXvSgYden7Iq3giWHVnVu3nXPU2pKbiEA337H\nKAtkWnqY4/6kpy4zZBVqSKLLIvHAUPRkhwJUl2WiAssNsxbaI25Y/l8olnzkAziD\nET1ZO21ztUBi+ZikzbzTdcWss17Me4hJLwqijW5DnWn0SK/wK+1sYGMhhMW5oloe\nJzWw/oGebddDvVr+YyDEeuinMbcIHG5DOnsHdiL2uB8LU1ESbY8GLXvI33qYZq8K\nItRKqUzLWjfGIGpYhgspWGOtePLzlVGZ4yFZjBpOoI3N4W4RsJTzdfz53xgLR1in\ni53ADJpvolMl4GJBiZC9hDBFU8beAJG3TpuSyQyWwQKBgQDaAPZD20ZihL6HHdkJ\nhDqkrS6jq/vVYZO9xDp194kJ/OlP8G+HafUsWy267p5+GL1/oA1n09XI8TtJD6Aq\no17lOmBDA1LEQUsyBElc4Qta7PnHgMlpDKFN2kyBXDiDRr9uPk2L/bP+M31PH1Aa\nJvPW45A8mmoVUU1tDkdckihqywKBgQC40QFXukdCvOqIlFFN/vsGIKI3oOBs7wNF\nBqonvckb1Mi/J/n9c4igS5VBRoCJ5KyNDDecPkz+li8usgGOZda+0Uuw0+f26OoZ\n+lSyoJzQDPSI94TqRu9EJzXKTkcu1EgDJoVvdLqEg4UlMFoDZj5E44QaQvdByN59\nJadO46scyQKBgBqJx3DXPqg7lJ4KKiOYbzMgye6uvwdoy31sZ9l4erw2h4htsoX/\nhhMSPmzlI3IiPN9OcpNTHFFL3HW2az8FiEXLZ3CA9RBjCm5aSzv9QU6eToHU0a/y\nE6jQjEOrIE4KzL704qfKCKFHT1fUbgHhAAoGxcY63fMeZJiEBnfkavslAoGAB/VH\n/4UP6HnGB5eLYLZoDL1+/rI4t7gbBe/hh9CORlqFQmXwR8+fxHc8uBDXHBjyrSgn\nhNzR5+yDn5kepAkSlCji/3xAcstYuUGkQATWe8bzCKrP2F6xkte1Yz63pfaSG0Cf\nYks+dNJM+2r3lUnjyIebep4Dk5uYqP7kE2uORLECgYBx1PPsBy1Uvp7RQ2xslIYE\np8mmu4R404GsFGEzHG+/ImpfzoDJr1MuRWN6OLYlopZZ2lERFWmjmqawmTORAUgN\n+KBuRo7Ntxe95Ck3PPPCUpwoqmTO6DIkxsLfdLGo46NThai/CB2pluJaNnTDNuiD\nR+myzVViXzPD1/VyDI4s9w==\n-----END PRIVATE KEY-----\n'
+    }),
+    databaseURL: 'https://onibase-aa4ed.firebaseio.com'
+});
+
+var app = admin.initializeApp();
+*/
